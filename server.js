d57e6889a99c3d14bf7646eb1022c564d45cb8c0
Cesco <crordaz@gmail.com>
2017-10-13 15:55:48 -0500

    Basic server established

---
+++ server.js
@@ -0,0 +1,27 @@
+//DEPENDENCIES
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//MONGOOSE CONNECTION
+mongoose.connection.openUri(process.env.DB_CONN, function(err,conn){
+	if(err){
+		console.log('Error connecting to Mongo DB');
+	}else{
+		console.log('Successfully connected to Mongo DB');
+	}
+})
+//app setup
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', function(req,res){
+	res.send('test');
+});
+
+app.listen(port,function(){
+	console.log(`Server listening on port ${port}`);
+});
