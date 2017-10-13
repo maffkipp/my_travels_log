6375333a4aad979e49eb75cf5fc24817212c167d
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-10-13 16:12:38 -0500

    set up basic ejs templates, added map

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0, lng: 0},
+    zoom: 2
+  });
+}
+
+$(document).ready(function() {
+  initMap();
+});
