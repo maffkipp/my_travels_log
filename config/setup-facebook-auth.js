d38e36a6ee02efa1cfaf7515d554da4834726fd6
CescoIV <crordaz@gmail.com>
2017-10-15 00:11:55 -0500

    min changes to setup

--- config/setup-facebook-auth.js
+++ config/setup-facebook-auth.js
@@ -13,31 +13,30 @@ function setupFacebookAuthStrategy(passport){
 	});
 
 	console.log('Setting up Facebook auth strategy');
+	
 	const strategyObj = {
 		clientID: process.env.FACEBOOK_APP_ID,
 		clientSecret: process.env.FACEBOOK_APP_SECRET,
 		callbackURL: 'http://localhost:3000/auth/facebook/callback',
 		enableProof: true,
 		profileFields: ['name','emails','photos'],
-		};
+	};
 
 	passport.use('facebook', new FacebookStrategy(strategyObj,
 		function(access_token, refresh_token,profile,done){
-			User.findOne({'fb.id': profile.id}, function(err,user){
+			Users.findOne({'fb.id': profile.id}, function(err,user){
 				if(err){
 					done(err);
 				}else if(user){
 					done(null,user);
 				}else{
 					const newUser = new User({
-						fb:{
-							id: profile.id,
-							access_token: access_token,
-							firstName: profile.name.givenName,
-							lastName: profile.name.familyName,
-							email: profile.emails[0].value,
-							profilePhoto: profile.photos[0].value,
-						}
+						id: profile.id,
+						access_token: access_token,
+						firstName: profile.name.givenName,
+						lastName: profile.name.familyName,
+						email: profile.emails[0].value,
+						profilePhoto: profile.photos[0].value,	
 					});
 
 					newUser.save(function(err){
