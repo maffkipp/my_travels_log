49c4f5e856a3981a89d624551573d0c45779ee50
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-10-16 14:44:16 -0500

    added styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
+// VARIABLES
 var map;
+var toggle = 0;
 
+
+// FUNCTION CALLS
+$(document).ready(function() {
+
+  console.log('application is running');
+  // initialize the map
+  initMap();
+  // assign event handler to display switch button
+  $('#display-switch').click( function() {
+    displaySwitch();
+  });
+
+});
+
+
+// FUNCTIONS
+
+// Initialize the map API
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
@@ -7,9 +27,18 @@ function initMap() {
   });
 }
 
-$(document).ready(function() {
-  console.log('application is running');
-  initMap();
-});
-
+// Switch between location form and list of saved locations
+function displaySwitch() {
+  if (toggle === 0) {
+    $('#city-list').fadeToggle(200, function() {
+      $('#location-form').fadeToggle(200);
+      toggle = 1;
+    });
+  } else {
+    $('#location-form').fadeToggle(200, function() {
+      $('#city-list').fadeToggle(200);
+      toggle = 0;
+    });
+  }
+}
 
