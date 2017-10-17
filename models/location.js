2ec9564d2352931eb0a469c2f46db5feeee4e914
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-10-17 13:20:52 -0500

    added coord lookup

--- models/location.js
+++ models/location.js
@@ -1,29 +1,16 @@
 const mongoose = require('mongoose');  //dependency does the heavier lifting to do the stuff on this page
-const googleMapsClient = require('@google/maps').createClient({
-  key: 'AIzaSyBV06Rqe2o_LP8qqrSeusRqs2VSNxAFMrU'
-});
+
 
 const LocationSchema = new mongoose.Schema({
-    long: String,
-    lat: String,
+    long: Number,
+    lat: Number,
     city: String,
     country: String,
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     visitDate: Date
 });
 
-LocationSchema.methods.findLatLong = function findLatLong() {
-  googleMapsClient.geocode({
-    address: `${this.city} ${this.country}`
-  }, (err, response) => {
-    if (err) {
-      res.status(500).send('Internal Server Error');
-    } else {
-      this.lat = response.json.results[0].geometry.location.lat.toString();
-      this.long = response.json.results[0].geometry.location.lng.toString();
-    }
-  });
-}
+
 
 const Location = mongoose.model('Location', LocationSchema); // lets make a model a thing
 
