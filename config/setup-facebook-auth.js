1ea140ed90f398f125f80d1c0645e96be0202ebb
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2017-10-19 15:32:08 -0500

    added responsive styles for mobile

--- config/setup-facebook-auth.js
+++ config/setup-facebook-auth.js
@@ -19,7 +19,7 @@ function setupFacebookAuthStrategy(passport){
 	const strategyObj = {
 		clientID: process.env.FACEBOOK_APP_ID,
 		clientSecret: process.env.FACEBOOK_APP_SECRET,
-		callbackURL: 'https://my-travels-log.herokuapp.com/auth/facebook/callback',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback',
 		enableProof: true,
 		profileFields: ['name','emails','photos'],
 	};
